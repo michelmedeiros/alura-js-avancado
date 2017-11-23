0160add9cfe9cfa173d737638a8ebc6401886b4a
Michel Medeiros <michel.tds@gmail.com>
2017-11-22 22:29:26 -0200

    Regex

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -5,12 +5,26 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         event.preventDefault();
+        let negociacao = this._criaNegociacao();
+        this._listaNegociacoes.adiciona(negociacao);
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+    }
+
+    _criaNegociacao() {
         let data = DateHelper.textoParaData(this._inputData.value);
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-        console.log(DateHelper.dataParaTexto(data));
+        return new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
     }
 }
